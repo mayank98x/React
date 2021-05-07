9739207a1e7863fa68c28a6151a2d59532dd1cf5
tyler mayank <tyler.mayank@ithands.com>
2021-05-07 22:56:55 +0530

    add:use javascript map function to loop over all the blogs

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,21 @@ import { useState } from 'react';
 
 const Home = () => {
 
-    const [name, setName] = useState('mayank');
-
-    const handleClick = () => {
-        setName('nirmal');
-        setAge(30);
-    }
-
-    const [age,setAge] = useState(25);
-
-
+    const [ blogs, setBlogs] = useState([
+        {title:'My new website' , body: ' lorem asdasd' , author: 'mayank' , id: 1},
+        {title:'welocome party website' , body: ' lorem asdasd' , author: 'nikita' , id: 2},
+        {title:'dev tools' , body: ' lorem asdasd' , author: 'ankita' , id: 3}
+    ])
     return ( 
         <div className="home">
-            <h2>Home page</h2>
-            <p>{ name } is {age} years old</p>
-            <button onClick={handleClick}>CLick me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+
+                </div>
+            ))}
+           
          </div>
      );
 }
